a7df06b632dc426dcdfa4af93d611d37a79a49d2
Arlind-Salihu <as47047@ubt-uni.net>
2021-12-23 04:50:21 +0100

    Shfaqja e E-Commerce ne Heroku.com si Production Mode

--- API/Startup.cs
+++ API/Startup.cs
@@ -61,12 +61,12 @@ namespace API
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
             app.UseCsp(opt => opt.BlockAllMixedContent()
-                                           .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
-                                           .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
-                                           .FormActions(s => s.Self())
-                                           .FrameAncestors(s => s.Self())
-                                           .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
-                                           .ScriptSources(s => s.Self().CustomSources("sha256-MCPYPK/WZlbUo7z/WdnPvU/1yIPhYV+RLv86GCl8OnY=")));
+                                 .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com","data:"))
+                                 .FormActions(s => s.Self())
+                                 .FrameAncestors(s => s.Self())
+                                 .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                                 .ScriptSources(s => s.Self().CustomSources("sha256-MCPYPK/WZlbUo7z/WdnPvU/1yIPhYV+RLv86GCl8OnY=")));
 
             if (env.IsDevelopment())
             {
